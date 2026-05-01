212b10ecfcbae3dd88a36494b4d35f7ae9a43393
Lucas De Sousa Campos <2416511802@aluno.imepac.edu.br>
2026-04-30 21:27:47 -0300

    Finalização das telas do app

--- app/src/main/java/br/com/faculdade/imepac/FormLogin.kt
+++ app/src/main/java/br/com/faculdade/imepac/FormLogin.kt
@@ -1,6 +1,8 @@
 package br.com.faculdade.imepac
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FormLogin : AppCompatActivity() {
@@ -8,7 +10,18 @@ class FormLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
 
-        // Comando para esconder o Toolbar (Barra superior roxa)
+        // Esconde a barra superior
         supportActionBar?.hide()
+
+        // Cria uma variável e encontra o texto de cadastro pelo ID [cite: 1705, 1723]
+        val linkFormCadastro = findViewById<TextView>(R.id.text_tela_cadastro)
+
+        // Cria a ação de clique no texto [cite: 1705, 1724]
+        linkFormCadastro.setOnClickListener {
+            // Prepara a navegação para a FormCadastro usando Intent [cite: 1720, 1725]
+            val telaCadastro = Intent(this, FormCadastro::class.java)
+            // Inicia a nova tela
+            startActivity(telaCadastro)
+        }
     }
 }
